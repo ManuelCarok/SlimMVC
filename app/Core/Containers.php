d9a08941616f8721a7a900c74625d068b5b7730f
Manuel Caroca <manuel@byteservices.cl>
2019-12-19 18:00:18 -0300

    pagina de error 404 y mensaje para error 405

--- app/Core/Containers.php
+++ app/Core/Containers.php
@@ -15,6 +15,26 @@ $app = new \Slim\App([
 
 $container = $app->getContainer();
 
+// Errores
+// Vista y API
+$container['notFoundHandler'] = function ($container) {
+    return function ($request, $response) use ($container) {
+        return $container->view->render($response, '/Error/404.twig');
+    };
+};
+
+// API: GET, POST, DELETE y PUT
+$container['notAllowedHandler'] = function ($container) {
+    return function ($request, $response, $methods) use ($container) {
+        $code = 405;
+        $resp = [
+            "statusText" => "Método no permitido ".implode(', ', $methods),
+            "status" => 405
+        ];
+        return $response->withJson($resp, $code)->withHeader('Content-Type', 'application/json');
+    };
+};
+
 // Twig
 $container['view'] = function($container) {
     $view = new \Slim\Views\Twig(dirname(__DIR__).'/Views', [
